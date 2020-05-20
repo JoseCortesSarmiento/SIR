ab657b919d87d238feee2ab0ec40b7d032f31af1
root <root@localhost.localdomain>
2020-05-19 19:59:54 -0500

    estructura final

--- vistas/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("KEY", "maskota");
-define("COD", "AES-128-ECB");
-
-define ("DATABASE_HOST", "localhost");
-define ("DATABASE_USER", "root");
-define ("DATABASE_PASS", "");
-define ("DATABASE_NAME", "sistemarest");
-?>
