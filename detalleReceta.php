0f67c0297b2a5371798d7eba54206d2503eb7c22
bego <begomontes7@gmail.com>
2020-05-28 11:46:59 -0500

    Termine vista detalles de receta, falta meter lo de la tabla de ingredientes ahi

--- detalleReceta.php
+++ detalleReceta.php
@@ -6,11 +6,6 @@ include 'global/sesion.php';
 include 'global/header.php';
 
 
-
-
-
-
-
 if (!empty($_GET['id_receta'])) { 
     $id_receta = $_REQUEST['id_receta']; 
     //  echo $id_proveedor;
@@ -30,15 +25,28 @@ $receta = $q->fetch(PDO::FETCH_ASSOC);
 
     
 <div class="jumbotron">
-    <h2 class="h1-responsive"><?=$receta['nombre_platillo']?></h2>
+    <h2 class="h1-responsive text-center" ><?= ucfirst($receta['nombre_platillo'])?></h2>
+    <br>
+    <br>
+    <div class="row">
+    <div class="col-sm">
+        <img src="<?= $receta['foto_empresa']?>" class="img-fluid" alt="Responsive image">
+    </div>
+
+    <div class="col-sm">
+        <img src="<?=$receta['foto_receta']?>" class="img-fluid" alt="Responsive image">
+    </div>
+        
+    </div>
+    <br>
+        <br>
     <div class="text-left">
-          <i class="far fa-file-alt fa-4x mb-3 animated rotateIn"></i>
           <p>
             <strong>Código:</strong> <?=$receta['codigo']?>
           </p>
 
           <p>
-            <strong>Categoria:</strong> <?=$receta['categoria']?>
+            <strong>Categoria:</strong> <?= ucfirst($receta['categoria'])?>
           </p>
 
 
@@ -47,23 +55,31 @@ $receta = $q->fetch(PDO::FETCH_ASSOC);
           </p>
           
     </div>
+    <hr>
+    <div class="row">
+    <div class="col-4"></div>
+        <div class="col-4 ">
+            <img src="<?=$receta['foto_logo']?>" class="img-fluid" alt="Responsive image">
+        </div>
+    <div class="col-4"></div>
+    </div>
     <hr class="my-2">
     <p>
         <strong>Presentación:</strong>
     </p>
-    <p class="lead">  <?=$receta['presentacion']?></p>
+    <p class="lead">  <?=ucfirst($receta['presentacion'])?></p>
 
     <hr class="my-2">
     <p>
         <strong>Mise en place:</strong>
     </p>
-    <p class="lead">  <?=$receta['mise_en_place']?></p>
+    <p class="lead">  <?=ucfirst($receta['mise_en_place'])?></p>
     <hr class="my-2">
 
     <p>
         <strong>Preparación:</strong>
     </p>
-    <p class="lead">  <?=$receta['preparacion']?></p>
+    <p class="lead">  <?=ucfirst($receta['preparacion'])?></p>
    
     <a class="btn btn-primary btn-lg" role="button" href="recetas.php">Ver todas</a>
 </div>
