7a78f1743d8ab527a51a7fa34dd690918db2ac1a
ImpersPv <manuelpr11@msn.com>
2020-05-29 21:16:19 -0500

    Se agrega la casilla historial al header

--- templates/head.php
+++ templates/head.php
@@ -34,6 +34,10 @@ include 'global/header.php';
       <li class="nav-item">
         <a class="nav-link" href="recetas.php">Recetas</a>
       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="historial.php">Historial</a>
+      </li>
     </ul>
 
     <ul class="navbar-nav ml-auto nav-flex-icons">
