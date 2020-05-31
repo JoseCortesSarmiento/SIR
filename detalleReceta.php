8585221212d07fd8f408798a998f1b0163311be8
ImpersPv <manuelpr11@msn.com>
2020-05-31 02:18:22 -0500

    Control de sesion terminado

--- detalleReceta.php
+++ detalleReceta.php
@@ -2,14 +2,12 @@
 include 'global/config.php';
 include 'global/conexion.php';
 include 'templates/head.php';
-include 'global/sesion.php';
 
 
-
-
-
-
-
+if($_SESSION['usuario']['rol']!=1){
+        
+        header("location:home.php");
+}
 
 
 
