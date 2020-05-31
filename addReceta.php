e9c26c5e4668a3b0518c34e2317dc51a2d07c84a
ImpersPv <manuelpr11@msn.com>
2020-05-31 01:15:51 -0500

    Manejo de sesion en addrecetas

--- addReceta.php
+++ addReceta.php
@@ -2,7 +2,10 @@
 include 'global/config.php';
 include 'global/conexion.php';
 include 'templates/head.php';
-include 'global/sesion.php';
+if($_SESSION['usuario']['rol']!=1){
+        
+        header("location:home.php");
+}
 
 
         // $sql8 ="SELECT COUNT(*) FROM recetas_articulos";
