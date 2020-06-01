fde5883e06531db3b4091acb45733fc2958b6833
root <root@localhost.localdomain>
2020-06-01 18:12:04 -0500

    Push para probar updates de Bego y Pavon y la base de Pavon

--- global/config.php
+++ global/config.php
@@ -10,11 +10,7 @@ define ("DATABASE_NAME", "sistemarest");
 
 //PGSQL
 $host='localhost';
-<<<<<<< HEAD
 $db = 'sistemarest';
-=======
-$db = 'sistemarest3';
->>>>>>> 53a7a14ff1bcde581d4b55094d763156c0b675fd
 $username = 'postgres';
 $password = 'postgres';
 
